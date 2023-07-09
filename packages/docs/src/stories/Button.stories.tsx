acfce5450daed7d0f1c9689da5ce2021aa572451
carloshkruger <carloshkruger@gmail.com>
2023-07-09 16:26:35 -0300

    chore: add new test color

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,7 +1,7 @@
 
 import { ArrowRight } from 'phosphor-react'
 import type { StoryObj, Meta } from '@storybook/react'
-import { Button, ButtonProps } from '@ignite-ui/react'
+import { Button, ButtonProps } from '@carloshkruger-ignite-ui/react'
 
 export default {
   title: 'Form/Button',
---
+++ packages/eslint-config/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @carloshkruger-ignite-ui/eslint-config
+
+## 2.0.0
+
+### Major Changes
+
+- Initial release
